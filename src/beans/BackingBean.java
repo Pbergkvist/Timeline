6cd72dc6fd54e5e33d753dfe01d96eae64ee311b
Niklas Gjörloff <niklas.gjorloff@gmail.com>
2018-03-06 20:33:07 +0100

    added login page amongst some other things

--- src/beans/BackingBean.java
+++ src/beans/BackingBean.java
@@ -12,6 +12,61 @@ public class BackingBean implements Serializable {
     @EJB
     private QueryBean theQuery;
     private List<EventEntity> event;
+    private String userName;
+    private String password;
+    private LoginEntity resultLE;
+    private boolean hide;
+
+    public boolean isHide() {
+        return hide;
+    }
+
+    public void setHide(boolean hide) {
+        this.hide = hide;
+    }
+
+    public void hideOrShow() {
+
+        if (!hide) {
+            hide = true;
+        } else {
+            hide = false;
+        }
+    }
+
+    public LoginEntity getResultLE() {
+        return resultLE;
+    }
+
+    public void setResultLE(LoginEntity resultLE) {
+        this.resultLE = resultLE;
+    }
+
+    public String searchUser() {
+        resultLE = theQuery.getUsers(userName, password);
+
+        if (resultLE != null) {
+            return "admin";
+        } else {
+            return "error";
+        }
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public QueryBean getTheQuery() {
         return theQuery;
--- src/beans/QueryBean.java
+++ src/beans/QueryBean.java
@@ -1,19 +1,36 @@
 package beans;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import java.util.List;
+
 @Stateless
 public class QueryBean {
-    @PersistenceContext(name="TimelinePU")
+    @PersistenceContext(name = "TimelinePU")
     EntityManager em;
 
     public List<EventEntity> getListOfEvents() {
         List<EventEntity> theQuery =
                 em.createQuery("select c from EventEntity c order by c.year", EventEntity.class)
-                .getResultList();
+                        .getResultList();
         List<EventEntity> result = theQuery;
         return result;
+    }
+
+    public LoginEntity getUsers(String user, String password) {
+        LoginEntity search = new LoginEntity();
+        try {
+            search = (LoginEntity)
+                    em.createQuery("select c from LoginEntity c where c.user=:User AND c.password=:Password")
+                            .setParameter("User", user)
+                            .setParameter("Password", password)
+                            .getSingleResult();
+            return search;
+        } catch (NoResultException ex) {
+            return null;
+        }
     }
 }
